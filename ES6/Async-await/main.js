1f8ff2e4f7d4306ef9bac3b3036eb5f81ceab340
Sandro Torres <sandrotorres.ti@gmail.com>
2020-02-06 06:17:25 -0300

    Async/Await Finalizado

---
+++ ES6/Async-await/main.js
@@ -0,0 +1,18 @@
+const minhaPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => { resolve('OK') }, 2000);
+});
+
+// async function executaPromise() {
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+//   console.log(await minhaPromise());
+
+// }
+
+const executaPromise = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+
+executaPromise();
