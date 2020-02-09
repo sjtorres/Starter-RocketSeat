8bd01fc3428baf681240abf63840683290f41e9c
Sandro Torres <sandrotorres.ti@gmail.com>
2020-02-09 06:18:03 -0300

    App Buscando Api do github finalizada

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -9,10 +9,22 @@ class App {
     this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
-  }
 
+  }
+  
   registerHandlers() {
     this.formEl.onsubmit = event => this.addRepository(event);
+  }
+  setLoading(loading = true) {
+     if (loading === true) {
+       let loadingEl = document.createElement('span');
+       loadingEl.appendChild(document.createTextNode('Carregando'));
+       loadingEl.setAttribute('id', 'loading');
+
+       this.formEl.appendChild(loadingEl);
+     } else {
+       document.getElementById('loading').remove();
+     }
   }
 
   async addRepository(event) {
@@ -23,20 +35,27 @@ class App {
     if (repoInput.length === 0) 
       return;
 
-    const response = await api.get(`/repos/${repoInput}`);
+    this.setLoading();
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
 
-    const { name, description, html_url, owner: { avatar_url } } = response.data;
+      const { name, description, html_url, owner: { avatar_url } } = response.data;
 
-    this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
-    });
+      this.repositories.push({
+        name,
+        description,
+        avatar_url,
+        html_url,
+      });
 
-    this.inputEl.value = '';
+      this.inputEl.value = '';
 
-    this.render();
+      this.render();
+    } catch (err) {
+      alert('O repositório não existe!');
+    }
+
+    this.setLoading(false);
   }
   render() {
     this.listEl.innerHTML = '';
