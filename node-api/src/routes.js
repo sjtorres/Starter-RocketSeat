ca3f6285c10bd18c4b3bb3290ba7303993d15c75
Sandro Torres <sandrotorres.ti@gmail.com>
2020-02-11 19:33:09 -0300

    Finalizado CRUD e Paginação

--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -5,5 +5,9 @@ const ProductController = require('./controllers/ProductControllers');
 
 // Criando a primeira rota
 routes.get("/products", ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post("/products", ProductController.store);
+routes.put("/products/:id", ProductController.update);
+routes.delete("/products/:id", ProductController.destroy);
 
 module.exports = routes;
